f11f2ce171fb24e91be510354cb944aa485229fb
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-07-11 17:15:08 -0700

    add intro for main menu with color

---
+++ lib/messages.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/MethodLength
+def main_menu
+  banner
+  intro
+  play_load
+end
+
+def banner
+  puts <<-'EOF'
+
+  $$\
+  $$ |
+  $$$$$$$\   $$$$$$\  $$$$$$$\   $$$$$$\  $$$$$$\$$$$\   $$$$$$\  $$$$$$$\
+  $$  __$$\  \____$$\ $$  __$$\ $$  __$$\ $$  _$$  _$$\  \____$$\ $$  __$$\
+  $$ |  $$ | $$$$$$$ |$$ |  $$ |$$ /  $$ |$$ / $$ / $$ | $$$$$$$ |$$ |  $$ |
+  $$ |  $$ |$$  __$$ |$$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$  __$$ |$$ |  $$ |
+  $$ |  $$ |\$$$$$$$ |$$ |  $$ |\$$$$$$$ |$$ | $$ | $$ |\$$$$$$$ |$$ |  $$ |
+  \__|  \__| \_______|\__|  \__| \____$$ |\__| \__| \__| \_______|\__|  \__|
+                                $$\   $$ |
+                                \$$$$$$  |
+                                 \______/
+  EOF
+end
+
+def intro
+  puts ''
+  puts ''
+  puts 'Welcome to Hangman'.bold.underline
+  puts ''
+end
+
+def play_load
+  puts "Enter: #{'1'.green.bold} to #{'Play'.green.bold}"
+  puts "       #{'2'.yellow.bold} to #{'Load'.yellow.bold}"
+end
+
+# rubocop:enable Metrics/MethodLength
